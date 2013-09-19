a1e2466adf961b79e115db09e2dffb3278ff7ef0
Christopher C Strelioff <chris.strelioff@gmail.com>
2013-09-19 13:35:05 -0700

    move create_summaryfile to util_infer_db.py

--- cmpy_bayes/__init__.py
+++ cmpy_bayes/__init__.py
@@ -1,8 +1,5 @@
 """cmpy_bayes
 
-Christopher C. Strelioff
-chris.strelioff@gmail.com
-
 A set of scripts to make use of Bayesian methods for inference of unifilar HMMs
 and epsilon-machines CMPy easier.
 
@@ -16,12 +13,11 @@ from .util_general import check_probability
 from .util_general import check_sr_tuple
 from .util_general import correct_neg_zero
 from .util_general import create_dir
-from .util_general import create_sample_summary_file
 from .util_general import read_datafile
 from .util_general import read_sample_dir
 
 from .util_infer_db import add_topologies_to_db
 from .util_infer_db import calc_probs_beta_db
+from .util_infer_db import create_sample_summary_file
 from .util_infer_db import prior_add_topologies_to_db
 from .util_infer_db import sample_db
-
